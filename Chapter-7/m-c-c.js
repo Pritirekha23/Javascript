9da2a1f30e6b56af17a0fa65963710e9ab568d1c
Pritirekha23 <learnpriti23@gmail.com>
2023-09-26 09:12:40 +0530

    adding files

---
+++ Chapter-7/m-c-c.js
@@ -0,0 +1,8 @@
+
+let id1=document.getElementById("id1")
+console.log("id1")
+console.log(id1.matches(".class"))
+
+console.log(span1.closest(".box"))
+console.log(span2.closest("#span2"))
+console.log(id1.contains(span1))
---
+++ Chapter-7/searching-element.js
@@ -0,0 +1,19 @@
+let ctitle=document.getElementsByClassName("card-title")[0]
+ctitle.style.color="red"
+
+let ctext=document.getElementsByClassName("card-text")[0]
+ctext.style.color="crimson"
+
+let botn=document.getElementsByClassName('btn btn-primary')[0]
+botn.style.color="black"
+
+document.querySelector(".this").style.background="crimson"
+
+
+// by id
+// id should be unique
+// let ct=document.getElementById("first-card-title")
+// ct.style.color="green"
+
+// let cts=document.querySelectorAll(".card-title")
+// console.log(cts)
