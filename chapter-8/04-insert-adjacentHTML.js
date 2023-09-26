baf53e9f3422c0da68009a9de9b2de8468917461
Pritirekha23 <learnpriti23@gmail.com>
2023-09-26 11:40:01 +0530

    adding files

--- chapter-8/04-insert-adjacentHTML.js
+++ chapter-8/04-insert-adjacentHTML.js
@@ -3,3 +3,4 @@ first.insertAdjacentHTML('beforeend','<div class="test"> beforeend</div>')
  first.insertAdjacentHTML('afterbegin','<div class="test"> afterbegin</div>')
  first.insertAdjacentHTML('afterend','<div class="test">afterend</div>')
  
+// first.remove()
---
+++ chapter-8/05-className and list.js
@@ -0,0 +1,7 @@
+first.className="text-black red"
+//const a=first.classList
+console.log(first.classList)
+first.classList.add("red")
+first.classList.toggle("red")
+
+//toggle : add karega agr nhi hoga or agr hoga ta hata dega
