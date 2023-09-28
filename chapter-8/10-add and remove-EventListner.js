25e5b96c7610cd1bc780779aef138dcd2dd843d8
Pritirekha23 <learnpriti23@gmail.com>
2023-09-28 15:13:31 +0530

    adding event listener

--- chapter-8/10-add and remove-EventListner.js
+++ chapter-8/10-add and remove-EventListner.js
@@ -1,4 +1,9 @@
 let x = function (e) {
+    
+    //console.log(e.target)
+    // In the place of e we can write anything (it is basically a eventpointer)
+    //console.log(e.type,e.clientX,e.clientY) -> It will give the information that where the mouse is moving.
+
     alert("Hello Preety!")
 }
 let y = function (e) {
