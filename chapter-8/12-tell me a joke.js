b7ca2992e1fbf0fe888e044b530a181e170b2c49
Pritirekha23 <learnpriti23@gmail.com>
2023-10-02 11:42:56 +0530

    adding a random joke

--- chapter-8/12-tell me a joke.js
+++ chapter-8/12-tell me a joke.js
@@ -1,173 +1,331 @@
-{
-    "error": false,
-    "amount": 10,
-    "jokes": [
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "Why is 6 afraid of 7 in hexadecimal Canada?",
-            "delivery": "Because 7 8 9 A?",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 7,
-            "safe": true,
-            "lang": "en"
+let myjoke= [
+    {
+        "category": "Pun",
+        "type": "single",
+        "joke": "Two fish in a tank. One turns to the other and says, \"Do you know how to drive this thing?\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "Programming is like sex.",
-            "delivery": "Make one mistake and you end up supporting it for the rest of your life.",
-            "flags": {
-                "nsfw": true,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": true
-            },
-            "id": 8,
-            "safe": false,
-            "lang": "en"
+        "id": 185,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "If Bill Gates had a dime for every time Windows crashed ... Oh wait, he does.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "single",
-            "joke": "ASCII silly question, get a silly ANSI.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 25,
-            "safe": true,
-            "lang": "en"
+        "id": 22,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "Why do Java programmers hate communism?",
-            "delivery": "They don't want to live in a classless society.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 53,
-            "safe": true,
-            "lang": "en"
+        "id": 184,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Debugging: Removing the needles from the haystack.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "Why do they call it hyper terminal?",
-            "delivery": "Too much Java.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 226,
-            "safe": true,
-            "lang": "en"
+        "id": 40,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Today I learned that changing random stuff until your program works is \"hacky\" and a \"bad coding practice\" but if you do it fast enough it's \"Machine Learning\" and pays 4x your current salary.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "How did the programmer die in the shower?",
-            "delivery": "He read the shampoo bottle instructions: Lather. Rinse. Repeat.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "safe": false,
-            "id": 266,
-            "lang": "en"
+        "id": 33,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Four engineers get into a car. The car won't start.\nThe Mechanical engineer says \"It's a broken starter\".\nThe Electrical engineer says \"Dead battery\".\nThe Chemical engineer says \"Impurities in the gasoline\".\nThe IT engineer says \"Hey guys, I have an idea: How about we all get out of the car and get back in\".",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "single",
-            "joke": "Have a great weekend!\nI hope your code behaves the same on Monday as it did on Friday.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 44,
-            "safe": true,
-            "lang": "en"
+        "id": 132,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "\"We messed up the keming again guys.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "Why did the JavaScript heap close shop?",
-            "delivery": "It ran out of memory.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 31,
-            "safe": true,
-            "lang": "en"
+        "id": 20,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Judge: \"I sentence you to the maximum punishment...\"\nMe (thinking): \"Please be death, please be death...\"\nJudge: \"Learn Java!\"\nMe: \"Damn.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "twopart",
-            "setup": "What do you get if you lock a monkey in a room with a typewriter for 8 hours?",
-            "delivery": "A regular expression.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 26,
-            "safe": true,
-            "lang": "en"
+        "id": 45,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Have a great weekend!\nI hope your code behaves the same on Monday as it did on Friday.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
         },
-        {
-            "category": "Programming",
-            "type": "single",
-            "joke": "Debugging is like being the detective in a crime movie where you're also the murderer at the same time.",
-            "flags": {
-                "nsfw": false,
-                "religious": false,
-                "political": false,
-                "racist": false,
-                "sexist": false,
-                "explicit": false
-            },
-            "id": 42,
-            "safe": true,
-            "lang": "en"
-        }
-    ]
-}
+        "id": 44,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Two C strings walk into a bar.\nThe bartender asks \"What can I get ya?\"\nThe first string says \"I'll have a gin and tonic.\"\nThe second string thinks for a minute, then says \"I'll take a tequila sunriseJF()#$JF(#)$(@J#()$@#())!*FNIN!OBN134ufh1ui34hf9813f8h8384h981h3984h5F!##@\"\nThe first string apologizes, \"You'll have to excuse my friend, he's not null-terminated.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 28,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 35,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+        "flags": {
+            "nsfw": true,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 30,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "My husband and I were happy for 20 years. And then we met.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 273,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "A SQL statement walks into a bar and sees two tables.\nIt approaches, and asks \"may I join you?\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 5,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "What does the MacBook have in common with Donald Trump?\n\nI would tell you....\nBut I don't compare apples to oranges.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": true,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 233,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "Never date a baker. They're too kneady.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 271,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "single",
+        "joke": "I bought some shoes from a drug dealer. I don't know what he laced them with, but I was tripping all day!",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 204,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 184,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "single",
+        "joke": "Oysters hate to give away their pearls because they are shellfish.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 90,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "single",
+        "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 24,
+        "safe": true,
+        "lang": "en"
+    }
+]
+
+// let index=Math.floor(Math.random()*(myjoke.length-1))
+
+// console.log(index)
+// jOke.innerHTML=myjoke[index].jOke
+let index = Math.floor(Math.random() * myjoke.length);
+console.log(index);
+let jOke = document.getElementById('jOke'); // assuming you have an HTML element with id 'jOke'
+jOke.innerHTML = myjoke[index].joke;
